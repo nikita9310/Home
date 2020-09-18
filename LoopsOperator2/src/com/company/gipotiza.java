f49cc2658b0c4bd379acef1d92ce6fa3a7fbad4e
Niko <shcheglov@list.ru>
2020-09-18 15:45:28 +0300

    Исправил гипотизу.

--- LoopsOperator2/src/com/company/gipotiza.java
+++ LoopsOperator2/src/com/company/gipotiza.java
@@ -3,10 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class gipotiza {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число ");
-        int number = scanner.nextInt();
+    public static boolean checkGipotiza(int number){
         while(number != 1){
             if(number%2 == 0)
                 number = number / 2;
@@ -14,7 +11,16 @@ public class gipotiza {
                 number = (number*3+1)/2;
             System.out.println(number);
 
+
         }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число ");
+        int number = scanner.nextInt();
+        checkGipotiza(number);
+
 
     }
 }
